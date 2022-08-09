c053e1d8dba4c8dbb5a191785acab1411a213406
Anel <xuro560@gmail.com>
2022-08-10 03:52:14 +0600

    event listeners added

--- js/script.js
+++ js/script.js
@@ -1,26 +1,42 @@
+    
+    let swiper = document.querySelector('.swiper');
+    let swiperWrapper = document.querySelector('.swiper-wrapper');
+    let swiperSlide = document.querySelectorAll('.swiper-slide');
+    let tabContainer = document.querySelectorAll('.tabs');
+    let tabSlide = document.querySelectorAll('.tabs__slide');
+
     let expandTextButton = document.querySelector('.section__button');
     let description = document.querySelector('.section__description');
+    let tabsButton = document.querySelector('.tabs__button');
+    
+    let tabContainerWidth = tabContainer.offsetWidth;
+    let windowWidth = window.innerWidth;
+    let tabsWidth = swiper.offsetWidth;
+
     description.classList.add('hide');
     description.classList.add('description-size');
 
-    expandTextButton.onclick = function() {
+    expandTextButton.addEventListener( 'click', function() {
+        
         description.classList.toggle('hide');
         description.classList.toggle('description-size');
-    };
 
+        const initialText = 'Читать далее';
+        if (expandTextButton.textContent.includes(initialText)) {
+            expandTextButton.textContent = 'Скрыть';
+            expandTextButton.classList.toggle('rotate');
+        } else {
+            expandTextButton.textContent = initialText;
+            expandTextButton.classList.toggle('rotate');
+        }
+    } );
 
-    let swiperContainer = document.querySelector('.swiper');
-    let swiperWrapper = document.querySelector('.swiper-wrapper');
-    let swiperSlide = document.querySelectorAll('.swiper-slide');
-    let swiperTabs = document.querySelectorAll('.tabs-swiper__tab');
-    let tabsButton = document.querySelector('.tabs__button');
+    //mobile window
+    if( windowWidth < 768 ) {
 
-    let mobileSize = true;
-
-
-    if (window.innerWidth < 767) {
-
-        new Swiper('.tabs-swiper', {
+        //change swiper view
+        new Swiper('.tabs', {
+            enabled: true,
             slidesPerView: 'auto',
             spaceBetween: 16,
             pagination: {
@@ -29,35 +45,52 @@
             },
 
         });
-        tabsButton.remove();
 
-    } else {
-        swiperContainer.className = '';
-        swiperContainer.classList.add('tabs');
+        if (!swiper.classList.contains('swiper') ) {
+            swiper.classList.toggle('swiper');
+            swiperWrapper.classList.toggle('swiper-wrapper');
+            for ( let slide of swiperSlide) {
+                slide.classList.toggle('swiper-slide');
+            }
+            
 
-        swiperWrapper.className = 'tabs-wrapper';
-
-        for (let i = 0; i < swiperSlide.length; i++) {
-            swiperSlide[i].className = 'tabs-container';
-            swiperTabs[i].className = '';
-            swiperTabs[i].classList.add('tab');
         }
 
-        if (swiperWrapper.className === 'tabs-wrapper') {
-            let tabsWidth = swiperWrapper.offsetWidth;
 
-            for (let i = 0; i < swiperSlide.length; i++) {
-                tabAmount = Math.floor( tabsWidth / swiperSlide[i].offsetWidth );
-            }
-            for (let i = tabAmount * 2; i < swiperSlide.length; i++ ) {
-                swiperSlide[i].classList.add('tab-hide');
-            }
-            tabsButton.onclick = function() {
+    //tab & desktop window
+    } else if ( windowWidth > 768 ) {
+        
+        swiper.classList.toggle('swiper');
+        swiperWrapper.classList.toggle('swiper-wrapper');
+        for ( let slide of swiperSlide) {
+            slide.classList.toggle('swiper-slide');
+        }
+        tabsWidth -= 70;
+        for (let i = 0; i < tabSlide.length; i++) {
+            
+            tabAmount = Math.floor( tabsWidth / tabSlide[i].offsetWidth );
+        }
 
+        for (let i = tabAmount * 2; i < tabSlide.length; i++ ) {
+            tabSlide[i].classList.add('tab-hide');
+        }
+        tabsButton.addEventListener( 'click', function() {
+
+            for (let i = tabAmount * 2; i < tabSlide.length; i++ ) {
+                tabSlide[i].classList.toggle('tab-hide');
+            }
+
+            const showAll = 'Показать все';
+            if (tabsButton.textContent.includes(showAll)) {
                 tabsButton.textContent = 'Скрыть';
-                for (let i = tabAmount * 2; i < swiperSlide.length; i++ ) {
-                    swiperSlide[i].classList.toggle('tab-hide');
-                }
-        }
-        }
+                tabsButton.classList.toggle('rotate');
+            } else {
+                tabsButton.textContent = showAll;
+                tabsButton.classList.toggle('rotate');
+
+            }
+            
+        });
+
+        
     }
